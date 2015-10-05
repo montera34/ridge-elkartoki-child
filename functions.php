cbc7582a0a301c8cdef5eef7f82f37286a6d344d
skotperez <voraginebramante@gmail.com>
2015-10-05 11:29:07 +0200

    registers eskola taxonomy and include it in masonry mosac

--- functions.php
+++ functions.php
@@ -17,4 +17,63 @@ function elkartoki_build_taxonomies() {
 		'rewrite' => array( 'slug' => 'eskola', 'with_front' => false )
 	));
 }
+
+/* RIDGE
+ * PARENT
+ * THEME
+ * REDEFINITIONS
+ */
+
+/**
+ * Generate the project thumb title for the portfolio masonry view
+ *
+ * @since 1.0
+ */
+ if( ! function_exists( 'ridge_the_thumb_title' ) ) :
+	 function ridge_the_thumb_title(){
+		 global $post;
+		 global $ttrust_config; // Grabs the isotope classes
+		 $skills_ucfirst    = array();
+
+		 // school child theme taxonomy terms
+		 $schools = get_the_terms( $post->ID, 'eskola' );
+		 if ( $schools && ! is_wp_error( $schools ) ) {
+		 	$schools_out = array();
+		 	foreach ( $schools as $school ) { $schools_out[] = $school->name; }
+		 } else { $schools_out = ""; }
+		 
+		 if( ! empty( $ttrust_config['isotope_names'] ) )
+			 $skills = $ttrust_config['isotope_names'];
+
+		 // Begin building the string with the project title
+		 $output = the_title( '<h2 class="entry-title">', "</h2>\n" );
+
+		 // Add skills if there are any
+		 if( ! empty( $skills ) ) {
+
+			 $output .= "\n<h3>";
+
+			 foreach( $skills as $skill ){
+
+				 $skills_ucfirst[] = ucfirst( esc_attr( $skill ) );
+
+			 }
+
+			 $output .= implode(', ', $skills_ucfirst );
+			 if ( $schools_out != '' ) {
+				$output .= '<span class="mosac-schools">';
+				$output .= __('Schools','elkartoki');
+				$output .= ': ';
+				$output .= implode(', ', $schools_out );
+				$output .= '</span>';
+			 }
+			 $output .= "</h3>";
+
+		 } // if
+
+		 echo $output;
+
+	 } // ridge_the_thumb_title
+ endif;
+
 ?>
